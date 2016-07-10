a3aabfa9f002eeb18d28145adf2700f9b1cc4027
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-07-10 20:07:19 +0100

    Tweaks to make walking skeleton pass.

--- src/funtest/groovy/steps/alive.groovy
+++ src/funtest/groovy/steps/alive.groovy
@@ -1,6 +1,9 @@
 package steps
 
+import org.spockframework.runtime.SpockAssertionError
+import spock.util.concurrent.PollingConditions
 import wslite.rest.RESTClient
+import wslite.rest.RESTClientException
 
 import static cucumber.api.groovy.EN.And
 
@@ -8,11 +11,19 @@ httpClient = new RESTClient("http://localhost:5050")
 httpClient.defaultContentTypeHeader = "application/json"
 httpClient.defaultCharset = "UTF-8"
 
+def pollingConditions = new PollingConditions(timeout: 5, delay: 0.5)
+
 And(~/^a running service$/) { ->
 }
 
 And(~/^the service is queried on "([^"]*)"$/) { String arg1 ->
-    response = httpClient.get(path: "/alive")
+    pollingConditions.eventually {
+        try {
+            response = httpClient.get(path: "/alive")
+        } catch (RESTClientException rce) {
+            throw new SpockAssertionError(rce.message)
+        }
+    }
 }
 
 And(~/^the service response status is (\d+)$/) { int status ->
