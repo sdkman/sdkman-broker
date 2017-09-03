00558a8104699c37b2f36a3bba921073f4f781ac
Marco Vermeulen <vermeulen.mp@gmail.com>
2017-09-03 11:59:12 +0100

    Add polling conditions to acceptance tests.

--- src/funtest/groovy/steps/persistence.groovy
+++ src/funtest/groovy/steps/persistence.groovy
@@ -1,9 +1,12 @@
 package steps
 
 import cucumber.runtime.CucumberException
+import spock.util.concurrent.PollingConditions
 
 import static cucumber.api.groovy.EN.And
 import static support.MongoHelper.*
+
+conditions = new PollingConditions(timeout: 10, initialDelay: 1.5, factor: 1.25)
 
 And(~/^an initialised database$/) { ->
     insertAliveInDb(db)
@@ -26,9 +29,13 @@ And(~/^an inaccessible database$/) { ->
 }
 
 And(~/^an audit entry for (.*) (.*) (.*) is recorded for (.*)$/) { String candidate, String version, String distribution, String platform ->
-    readAuditEntry(db, candidate, version, distribution, platform).orElseThrow { -> new CucumberException("No audit entry found for $candidate $version $distribution for $platform") }
+    conditions.eventually {
+        readAuditEntry(db, candidate, version, distribution, platform).orElseThrow { -> new CucumberException("No audit entry found for $candidate $version $distribution for $platform") }
+    }
 }
 
 And(~/^an audit entry for (.*) (.*) (.*) is not recorded for (.*)$/) { String candidate, String version, String distribution, String platform ->
-    readAuditEntry(db, candidate, version, distribution, platform).ifPresent { x -> new CucumberException("Audit entry found for $candidate $version $distribution for $platform") }
+    conditions.eventually {
+        readAuditEntry(db, candidate, version, distribution, platform).ifPresent { x -> new CucumberException("Audit entry found for $candidate $version $distribution for $platform") }
+    }
 }
