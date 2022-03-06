c6618b1ca83fd7127227fed9c0954a75d5af4bd0
Marco Vermeulen <vermeulen.mp@gmail.com>
2022-03-06 14:59:51 +0000

    Introduce NativeBinaryDownloadHandler with supporting classes.

---
+++ src/main/java/io/sdkman/broker/rust/NativeBinaryDownloadConfig.java
@@ -0,0 +1,24 @@
+package io.sdkman.broker.rust;
+
+public class NativeBinaryDownloadConfig {
+    private final String protocol = null;
+    private final String host = null;
+    private final String uri = null;
+    private final String name = null;
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/io/sdkman/broker/rust/NativeBinaryDownloadHandler.java
@@ -0,0 +1,52 @@
+package io.sdkman.broker.rust;
+
+import io.sdkman.broker.NativeTarget;
+import io.sdkman.broker.audit.AuditEntry;
+import io.sdkman.broker.audit.AuditRepo;
+import io.sdkman.broker.binary.RequestDetails;
+import io.sdkman.broker.download.Platform;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
+
+import javax.inject.Inject;
+
+import static java.lang.String.format;
+
+public class NativeBinaryDownloadHandler implements Handler {
+
+    private static final Logger logger = LoggerFactory.getLogger(NativeBinaryDownloadHandler.class);
+
+    private final NativeBinaryDownloadConfig config;
+    private final AuditRepo auditRepo;
+
+    @Inject
+    public NativeBinaryDownloadHandler(NativeBinaryDownloadConfig config, AuditRepo auditRepo) {
+        this.config = config;
+        this.auditRepo = auditRepo;
+    }
+
+    @Override
+    public void handle(Context ctx) throws Exception {
+        RequestDetails.of(ctx).ifPresentOrElse(details -> {
+            logger.info("Received native download request for: " + details);
+            String version = details.getVersion();
+            Platform.of(details.getPlatform()).ifPresentOrElse(platform ->
+                            NativeTarget.of(platform).ifPresentOrElse((target -> {
+                                        record(details, platform.id(), platform.name());
+                                        ctx.redirect(format(prepareRemoteBinaryUrl(), version, version, target.getTriple()));
+                                    }),
+                                    () -> ctx.clientError(404)),
+                    () -> ctx.clientError(404));
+        }, () -> ctx.clientError(400));
+    }
+
+    private String prepareRemoteBinaryUrl() {
+        return config.getProtocol() + "://" + config.getHost() + config.getUri() + config.getName();
+    }
+
+    private void record(RequestDetails details, String platform, String distribution) {
+        auditRepo.record(AuditEntry.of(details.getCommand(), "sdkman_native", details.getVersion(), details.getHost(), details.getAgent(), platform, distribution));
+    }
+}
